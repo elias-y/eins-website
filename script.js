3c4753eea8894e586a15c6d8432002994ad0f0f3
elias-y <39983193+elias-y@users.noreply.github.com>
2025-05-04 19:55:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-function handleLogin(event) {
-  event.preventDefault(); // Verhindert Seitenreload
 
-  const email = document.getElementById("email").value;
-  const passwort = document.getElementById("password").value;
-
-  // Fake-Daten zum Testen
-  if (email === "test@example.com" && passwort === "123456") {
-    alert("Login erfolgreich 🎉");
-    // Weiterleitung z. B.: window.location.href = "dashboard.html";
-  } else {
-    alert("Falsche Zugangsdaten ❌");
-  }
-}
